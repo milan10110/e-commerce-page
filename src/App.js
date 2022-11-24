f6b83ac425b633d4212ef74eafd2e40bc4a744a8
milan10110 <meelaanvaghasiya00@gmail.com>
2022-11-24 23:28:22 +0530

    added basic routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+      </Routes>
     </div>
   );
 }
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -3,7 +3,12 @@ import Directory from "../../components/directory/directory.component";
 
 import "./homepage.styles.scss";
 
-function HomePage() {
+function HomePage(props) {
+  console.log(
+    "🚀 ~ file: homepage.component.jsx ~ line 7 ~ HomePage ~ props",
+    props
+  );
+
   return (
     <div className="homepage">
       <Directory />
