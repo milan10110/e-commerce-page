14df899c4ab6dc16fb1bc772c7c4baed4d8f3944
milan10110 <meelaanvaghasiya00@gmail.com>
2022-11-25 17:55:40 +0530

    updating app with shop page and collection preview

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shoppage/shop.component";
 
 function App() {
   return (
     <div>
       <Routes>
         <Route exact path="/" element={<HomePage />} />
+        <Route path="/shop" element={<ShopPage />} />
       </Routes>
     </div>
   );
