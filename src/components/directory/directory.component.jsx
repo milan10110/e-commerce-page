3fbb7942521bc829ba3d8034a31c67a1ec8297d8
milan10110 <meelaanvaghasiya00@gmail.com>
2022-12-29 10:32:06 +0530

    name change in directory

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -5,7 +5,7 @@ import { createStructuredSelector } from "reselect";
 import { selectDirectorySections } from "../../redux/directory/directory.selectors";
 
 import MenuItem from "../menu-item/menu-item.component";
-import "./directory.component.scss";
+import "./directory.styles.scss";
 
 function Directory({ sections }) {
   return (
